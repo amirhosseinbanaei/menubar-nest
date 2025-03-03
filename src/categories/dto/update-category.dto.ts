997e515fa68d344a7a9e3e84853372c74d18e45c
amir hossein banaei <developer.amirhossein@gmail.com>
2025-03-03 14:33:27 +0330

    Dto for updating category

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
+import { IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+
+export class UpdateCategoryOrdersDto {
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  orders: {
+    id: number;
+    order: number;
+  }[];
+}
