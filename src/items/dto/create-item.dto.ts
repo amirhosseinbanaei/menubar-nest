42c10493fbf4b10616f951eb883eea5ba1b0f52c
amir hossein banaei <developer.amirhossein@gmail.com>
2025-03-03 14:47:19 +0330

    DTo for creating item

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,55 @@
-export class CreateItemDto {}
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { CreateTranslationDto } from '../../languages/dto/create-translation.dto';
+
+export class CreateItemDto {
+  @IsNumber()
+  @Type(() => Number)
+  restaurant_id: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  branch_id: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  category_id: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  subcategory_id?: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  price: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  discount?: number;
+
+  @IsBoolean()
+  @Type(() => Boolean)
+  is_hide: boolean;
+
+  @IsBoolean()
+  @Type(() => Boolean)
+  is_available: boolean;
+
+  image?: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateTranslationDto)
+  @Transform(({ value }) =>
+    typeof value === 'string' ? JSON.parse(value) : value,
+  )
+  translations: CreateTranslationDto[];
+}
