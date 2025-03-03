ece3476629df274499cb758b5407b59bf33829be
amir hossein banaei <developer.amirhossein@gmail.com>
2025-03-03 14:33:52 +0330

    final category entity

--- src/categories/entities/category-translation.entity.ts
+++ src/categories/entities/category-translation.entity.ts
@@ -1,21 +1,32 @@
-import { Entity, PrimaryColumn, Column, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Category } from './category.entity';
 import { Language } from '../../languages/entities/language.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class CategoryTranslation {
-  @PrimaryColumn()
-  category_id: number;
-
-  @PrimaryColumn()
-  language_id: number;
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
 
   @Column({ length: 255 })
-  translated_name: string;
+  name: string;
 
-  @ManyToOne(() => Category, (category) => category.translations)
+  @ManyToOne(() => Category, (category) => category.translations, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'category_id' })
   category: Category;
 
-  @ManyToOne(() => Language, (language) => language.categoryTranslations)
+  @ManyToOne(() => Language, (language) => language.categoryTranslations, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'language_code' })
   language: Language;
 }
--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,12 +1,12 @@
 import {
   Entity,
-  PrimaryGeneratedColumn,
   Column,
   ManyToOne,
   OneToMany,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Restaurant } from '../../restaurants/entities/restaurant.entity';
-import { Subcategory } from './subcategory.entity';
+import { Subcategory } from '../../subcategories/entities/subcategory.entity';
 import { CategoryTranslation } from './category-translation.entity';
 import { Item } from '../../items/entities/item.entity';
 
@@ -16,17 +16,27 @@ export class Category {
   id: number;
 
   @Column({ length: 512 })
-  icon: string;
+  image: string;
+
+  @Column({ nullable: false, default: 0 })
+  order: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
 
   @ManyToOne(() => Restaurant, (restaurant) => restaurant.categories)
   restaurant: Restaurant;
 
-  @OneToMany(() => Subcategory, (subcategory) => subcategory.category)
+  @OneToMany(() => Subcategory, (subcategory) => subcategory.category, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
   subcategories: Subcategory[];
 
   @OneToMany(
     () => CategoryTranslation,
     (categoryTranslation) => categoryTranslation.category,
+    { cascade: true, onDelete: 'CASCADE' },
   )
   translations: CategoryTranslation[];
 
