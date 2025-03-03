043fec84685942e3493f35131e687118efebf0b1
amir hossein banaei <developer.amirhossein@gmail.com>
2025-03-03 14:32:31 +0330

    DTO for creating category

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,23 @@
-export class CreateCategoryDto {}
+import { IsArray, IsNumber, ValidateNested } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { CreateTranslationDto } from '../../languages/dto/create-translation.dto';
+
+export class CreateCategoryDto {
+  @IsNumber()
+  @Type(() => Number)
+  restaurant_id: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  branch_id: number;
+
+  image?: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateTranslationDto)
+  @Transform(({ value }) =>
+    typeof value === 'string' ? JSON.parse(value) : value,
+  )
+  translations: CreateTranslationDto[];
+}
