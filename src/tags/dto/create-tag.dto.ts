f10d6f90b8726e6e91bbef6c973e65d0bc8f4adf
amir hossein banaei <developer.amirhossein@gmail.com>
2025-03-04 20:25:55 +0330

    DTo for add tags

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTagDto {}
+import { Transform, Type } from 'class-transformer';
+import { IsArray, IsNumber, ValidateNested } from 'class-validator';
+import { CreateTranslationDto } from 'src/languages/dto/create-translation.dto';
+
+export class CreateTagDto {
+  @IsNumber()
+  @Type(() => Number)
+  restaurant_id: number;
+
+  image?: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateTranslationDto)
+  @Transform(({ value }) =>
+    typeof value === 'string' ? JSON.parse(value) : value,
+  )
+  translations: CreateTranslationDto[];
+}
