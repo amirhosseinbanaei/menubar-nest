bb434ed20dd82171963d7caf0f426e35b9ae9a08
amir hossein banaei <developer.amirhossein@gmail.com>
2025-03-04 19:38:46 +0330

    DTo for responsing items

---
+++ src/items/dto/response-item.dto.ts
@@ -0,0 +1,52 @@
+import { Expose, Transform, Type } from 'class-transformer';
+import { DetailTranslationDto } from '../../languages/dto/translation.dto';
+import { SubCategoryResponseDto } from '../../subcategories/dto/response-subcategory.dto';
+import { CategoryResponseDto } from 'src/categories/dto/response-category.dto';
+
+export class ItemResponseDto {
+  @Expose()
+  id: number;
+
+  @Expose({ name: 'category_id' })
+  @Type(() => CategoryResponseDto)
+  @Transform(({ obj }) => obj.category?.id ?? null, {
+    toClassOnly: true,
+  })
+  category_id: number | null;
+
+  @Expose({ name: 'subcategory_id' })
+  @Type(() => SubCategoryResponseDto)
+  @Transform(({ obj }) => obj.subcategory?.id ?? null, {
+    toClassOnly: true,
+  })
+  subcategory_id: number | null;
+
+  @Expose()
+  @Type(() => DetailTranslationDto)
+  translations: DetailTranslationDto[];
+
+  @Expose()
+  image: string;
+
+  @Expose()
+  order: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  discount: number;
+
+  @Expose()
+  is_hide: number;
+
+  @Expose()
+  is_available: number;
+
+  @Expose()
+  created_at: Date;
+
+  constructor(partial: Partial<ItemResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
